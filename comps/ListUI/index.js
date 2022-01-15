916be60896b524343b3cf29d0d5e32f3a44f9d8f
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-01-15 15:18:49 -0800

    update listUI

--- comps/ListUI/index.js
+++ comps/ListUI/index.js
@@ -2,13 +2,20 @@ import Card from '../Card'
 
 const ListUI = ({
     lists=[],
-    listText='List Title'
+    listText='List Title',
+    loadMore =()=>{console.log('Load more')}
 }) => {
 
 
     return (
         <div className='lists_wrap'>
-            <h1>{listText}</h1>
+            <div className='lists_header'>
+                <h1>{listText}</h1>
+                <div 
+                    onClick={loadMore}
+                    className='load_more'
+                >Load more &#9654;&#9654;</div>
+            </div>
             <div className='card_wrap'>
                 <Card/>
                 <Card/>
