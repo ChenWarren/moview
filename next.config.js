a57ef344d01a89228be821f78ad96cee571055fe
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-05-30 21:09:28 -0700

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_KEY: process.env.API_KEY,
+    BASE_URL: process.env.BASE_URL,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL
+  }
 }
+
+module.exports = nextConfig
