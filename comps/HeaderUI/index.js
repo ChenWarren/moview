182d9be8a13fd54ea8b033e3a97ef54bb2696517
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-05-31 12:30:16 -0700

    update hamburger menu

--- comps/HeaderUI/index.js
+++ comps/HeaderUI/index.js
@@ -1,25 +1,51 @@
 // Header UI
+import {useState} from 'react'
 import { useRouter } from "next/router"
+import Image from "next/image"
+import HamburgerMenu from '../HamburgerMenu'
 import MenuItem from "../MenuItem"
 import { Categories } from "../../pages/index"
+import MenuIcon from '../../public/burger_menu.svg'
 
-const HeaderUI = () => {
+const HeaderUI = ({
+
+}) => {
     const router = useRouter()
+    const [showMenu, setShowMenu]= useState(false)
+
+    const handleClick = () => {
+        setShowMenu(true)
+    }
 
     return (
-        <div className="header_default">
-            <div className="navbar">
-                <img 
-                    className="logo" src="/moview_logo.svg"
-                    onClick={()=> router.push('/')}
-                />
-                <div className="menu">
-                    { Categories.map((item) => {
-                        return <MenuItem key={item.id} menuText={item.menu} rout={item.rout}/>
-                    })}
+        <>
+            {showMenu && <HamburgerMenu setShowing={setShowMenu}/>} 
+            <div className="header_default">
+                <div className="navbar">
+                    <img 
+                        className="logo" 
+                        src="/moview_logo.svg"
+                        onClick={()=> router.push('/')}
+                    />
+                    <img
+                        className='logo-small' 
+                        src='/moview_icon.svg'
+                        onClick={()=> router.push('/')}
+                    />
+                    {!showMenu &&
+                        <div className="hamburger" onClick={handleClick}>
+                            <Image src={MenuIcon} width={30}/>
+                        </div>
+                    }
+                    
+                    <div className="menu">
+                        { Categories.map((item) => {
+                            return <MenuItem key={item.id} menuText={item.menu} rout={item.rout}/>
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
